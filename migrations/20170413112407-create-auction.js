ad1e505f9efd5d6f09015f8e74d9e8b1f00f81f5
Eri Irawan <eryirawan91@gmail.com>
2017-04-13 19:20:59 +0700

    add migration for auctionN

---
+++ migrations/20170413112407-create-auction.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Auctions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      min_price: {
+        type: Sequelize.INTEGER
+      },
+      max_price: {
+        type: Sequelize.INTEGER
+      },
+      kelipatan_bid: {
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      start_date: {
+        type: Sequelize.DATE
+      },
+      end_date: {
+        type: Sequelize.DATE
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Auctions');
+  }
+};
