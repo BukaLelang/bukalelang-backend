3fe99e13ce1f3808a603f05c03ef1844e46d2964
Eri Irawan <eryirawan91@gmail.com>
2017-04-13 19:23:17 +0700

    modify/add association

--- models/bid.js
+++ models/bid.js
@@ -7,7 +7,8 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Bid.belongsTo(models.User)
+        Bid.belongsTo(models.Auction)
       }
     }
   });
