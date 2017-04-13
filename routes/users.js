c193bdd3f074d3bf83119ee418885f19146fb152
Diky Arga <dikyarga.id@gmail.com>
2017-04-13 16:41:27 +0800

    Setup APIDOCS

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+/**
+ * @api {get} /users List all users
+ * @apiGroup Users
+ * @apiSuccess {Object[]} User's list
+ * @apiSuccess {Date} users.updated_at Update's date
+ * @apiSuccess {Date} users.created_at Register's date
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "id": 1,
+ *      "username": "Diky Arga",
+ *      "email": 'dikyarga.id@gmail.com',
+ *      "updated_at": "2016-02-10T15:46:51.778Z",
+ *      "created_at": "2016-02-10T15:46:51.778Z"
+ *    }]
+ * @apiErrorExample {json} List error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
