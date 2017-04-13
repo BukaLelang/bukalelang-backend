25a6ec184d99613eeddc546d7b2ac9dde313e618
Eri Irawan <eryirawan91@gmail.com>
2017-04-13 19:22:54 +0700

    modify/add assosiation

--- models/auction.js
+++ models/auction.js
@@ -2,16 +2,19 @@
 module.exports = function(sequelize, DataTypes) {
   var Auction = sequelize.define('Auction', {
     title: DataTypes.STRING,
-    category: DataTypes.STRING,
-    max_price: DataTypes.INTEGER,
     min_price: DataTypes.INTEGER,
+    max_price: DataTypes.INTEGER,
     kelipatan_bid: DataTypes.INTEGER,
     location: DataTypes.STRING,
+    start_date: DataTypes.DATE,
+    end_date: DataTypes.DATE,
+    CategoryId: DataTypes.INTEGER,
     UserId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Auction.hasMany(models.Bid)
+        Auction.belongsTo(models.Category)
       }
     }
   });
