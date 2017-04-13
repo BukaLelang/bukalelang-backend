6923e94356028d998a34e6444424105cb96020a4
Eri Irawan <eryirawan91@gmail.com>
2017-04-13 19:23:45 +0700

    modify/add association

--- models/user.js
+++ models/user.js
@@ -12,7 +12,8 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        User.hasMany(models.Bid)
+        User.hasMany(models.Auction)
       }
     }
   });
