10d5e268df36dd0c875a4342847c3b79e28d9fe6
Diky Arga <dikyarga.id@gmail.com>
2017-04-13 18:16:02 +0800

    Server UP Testing done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/**
+ * @api {get} /ping Make sure server is runnig well
+ * @apiGroup Status
+ * @apiSuccess {String} status Status Server
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "status": "up",
+ *    }]
+ * @apiErrorExample {json} List error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+router.get('/ping', function(req, res, next){
+  res.json({
+    status: "up"
+  })
+})
+
 module.exports = router;
