99e223b55c3352e36b3e690877daac2ae20573f6
Diky Arga <dikyarga.id@gmail.com>
2017-04-16 18:52:35 +0800

    make image uploader great again

--- helpers/imageUploader.js
+++ helpers/imageUploader.js
@@ -1,18 +1,32 @@
 let axios = require('axios')
+var request = require('request');
+let fs = require('fs')
 
 let blEndPoint = 'https://api.bukalapak.com/v2/images.json'
 
 module.exports = {
-  uploadToBukaLapak: (req, res) => {
-    console.log('isi request yang isi nya image : ', req.file);
-    let formData = req.body;
-    console.log('form data', formData);
-    axios({
-      method: 'post',
-      auth: {
-        username: req.body.bukalapakId,
-        password: req.body.token
-      }
+  uploadToBukaLapak: (bukalapakId, token) => {
+    return new Promise((resolve, reject) => {
+
+      var username = bukalapakId,
+      password = token,
+      url = 'https://' + username + ':' + password + '@api.bukalapak.com/v2/images.json';
+      var formData = {
+        // Pass data via Streams
+        //  with dummy data
+        file: fs.createReadStream(__dirname + '/images/sample-image.jpg'),
+      };
+      request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
+        // console.log('Upload successful!  Server responded with:', body);
+        if (err) {
+          reject('upload failed:' + err);
+        }
+        if (body.status == 'ERROR') {
+          reject('Error saat upload image : ' + body.message)
+        } else {
+          resolve(JSON.parse(body))
+        }
+      });
     })
   }
 }
