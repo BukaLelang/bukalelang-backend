3d4acb6cb8d603623753baedce3057fa30111d82
Diky Arga <dikyarga.id@gmail.com>
2017-04-17 16:14:45 +0800

    make all bid test working

--- test/bid.js
+++ test/bid.js
@@ -1,13 +1,15 @@
 var chai = require('chai')
 var chaiHttp = require('chai-http')
 require('dotenv').config()
+let app = require('../app')
 
 var should = chai.should()
 chai.use(chaiHttp)
 
+
 let models = require('../models')
 
-let serverHost = 'http://localhost:3000'
+let serverHost = app
 
 describe('Bid Test', () => {
   describe('Bidding', () => {
@@ -26,7 +28,7 @@ describe('Bid Test', () => {
             if (err) {
               done(err)
             } else {
-              console.log('isi res di test : ', res.body);
+              // console.log('isi res di test : ', res.body);
               res.should.have.status(200);
               res.should.be.json;
               res.body.success.should.to.equal(true)
@@ -51,56 +53,101 @@ describe('Bid Test', () => {
         }
       })
     })
-    xit('Should be return all field / property when trying to bid a auction', (done) => {
-      chai.request(serverHost).post('/bids/').send({
-        username: process.env.BUKALAPAK_ACCOUNT_USERNAME_DEV,
-        password: process.env.BUKALAPAK_ACCOUNT_PASSWORD_DEV
-      }).end((err, res) => {
-        if (err) {
-          done(err)
+    it('Should be return all field / property when trying to bid a auction', (done) => {
+      models.Auction.findAll().then(auctions => {
+        const auctionsLength = auctions.length
+        if (auctions.length != 0) {
+          // console.log('ambil auction terakhir : ', auctions);
+          // console.log('id terakhir ? ', auctions[auctionsLength - 1].id);
+          chai.request(serverHost).post('/bids/').send({
+            userId: 1,
+            token: process.env.BUKALAPAK_TOKEN,
+            auctionId: auctions[auctionsLength - 1].id,
+            nextBid: 140000
+          }).end((err, res) => {
+            if (err) {
+              done(err)
+            } else {
+              res.should.have.status(200);
+              res.should.be.json;
+              res.body.success.should.to.equal(true)
+              res.body.should.have.property('id')
+              res.body.should.have.property('auctionId')
+              res.body.should.have.property('current_price')
+              res.body.should.have.property('minimum_next_bidding')
+              res.body.should.have.property('success')
+              res.body.should.have.property('message')
+              // delete bid after test
+              models.Bid.findById(res.body.id).then(bid => {
+                if (bid) {
+                  bid.destroy({
+                    where: {
+                      id : bid.id
+                    }
+                  }).then(() => {
+                    console.log('delete bid after test success');
+                    done()
+
+                  })
+                }
+              })
+            }
+          });
         } else {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.success.should.to.equal(true)
-          res.body.should.have.property('id')
-          res.body.should.have.property('auctionId')
-          res.body.should.have.property('current_price')
-          res.body.should.have.property('minimum_next_bidding')
-          res.body.should.have.property('success')
-          res.body.should.have.property('message')
-          done()
+          console.log('tidak ada auctions di DB');
         }
-      });
+      })
     })
-    xit('Should be return status false when trying to bid a auction with wrong username or password', (done) => {
-      chai.request(serverHost).post('/bids/').send({
-        username: process.env.BUKALAPAK_ACCOUNT_USERNAME_DEV,
-        password: 'passwordAsalAsalan'
-      }).end((err, res) => {
-        if (err) {
-          done(err)
+    it('Should be return status false when trying to bid a auction with wrong parameter', (done) => {
+      models.Auction.findAll().then(auctionsFour => {
+        const auctionsLengthFour = auctionsFour.length
+        if (auctionsLengthFour != 0) {
+          // console.log('ambil auction terakhir : ', auctions);
+          // console.log('id terakhir ? ', auctions[auctionsLength - 1].id);
+          chai.request(serverHost).post('/bids/').send({
+            userId: 1,
+            token: process.env.BUKALAPAK_TOKEN,
+            auctionId: 2831983,
+            nextBid: 140000
+          }).end((err, hasil) => {
+            if (err) {
+              done(err)
+            } else {
+
+              hasil.should.have.status(200);
+              hasil.should.be.json;
+              hasil.body.success.should.to.equal(false)
+              done()
+            }
+          });
         } else {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.success.should.to.equal(false)
-          done()
+          console.log('tidak ada auctions di DB');
         }
-      });
+      })
     })
-    xit('Should be return current_price = null when trying to bid a auction with wrong username or password', (done) => {
-      chai.request(serverHost).post('/bids/').send({
-        username: process.env.BUKALAPAK_ACCOUNT_USERNAME_DEV,
-        password: 'passwordAsalAsalan'
-      }).end((err, res) => {
-        if (err) {
-          done(err)
+    it('Should be return current_price = null when trying to bid a auction with wrong parameter', (done) => {
+      models.Auction.findAll().then(auctions => {
+        const auctionsLength = auctions.length
+        if (auctionsLength != 0) {
+          chai.request(serverHost).post('/bids/').send({
+            userId: 1,
+            token: process.env.BUKALAPAK_TOKEN,
+            auctionId: 28891331983,
+            nextBid: 140000
+          }).end((err, res) => {
+            if (err) {
+              done(err)
+            } else {
+              res.should.have.status(200);
+              res.should.be.json;
+              should.equal(res.body.current_price, null);
+              done()
+            }
+          });
         } else {
-          res.should.have.status(200);
-          res.should.be.json;
-          should.equal(res.body.current_price, null);
-          done()
+          console.log('tidak ada auctions di DB');
         }
-      });
+      })
     })
   })
 })
