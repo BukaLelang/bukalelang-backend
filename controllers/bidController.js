af63696265c6945eb041cfa9778c43e27e8bb4f6
Diky Arga <dikyarga.id@gmail.com>
2017-04-17 10:41:41 +0800

    checkpoin bidding controller

--- controllers/bidController.js
+++ controllers/bidController.js
@@ -2,6 +2,7 @@ let axios = require('axios')
 
 const models = require('../models')
 let imageUploader = require('../helpers/imageUploader')
+let bidChacker = require('../helpers/bidChacker')
 
 let blEndPoint = 'https://api.bukalapak.com/v2/'
 
@@ -19,6 +20,26 @@ var finalResult = {
 module.exports = {
   bid: (req, res) => {
     console.log('isi request : ', req.body);
+    // cek user nya ada ngak ?
+    models.User.findById(req.body.userId).then(user => {
+      console.log('dapet user : ', user);
+      if (!user) {
+        // kalo ada, baru cek saldonya
+        bidChacker.checkBalance(user.bukalapakId, user.bl_token).then(balance => {
+          console.log('isi saldo pengguna : ', balance);
+          // cek saldonya lebih tinggi dari bid yang udah ada belum
+          bidChacker.isMoreThanHighestBid(req.body.auctionId, req.body.nextBid).then(responseAfterIsMoreThanHighestBid => {
+            
+          })
+        })
+      } else {
+        console.log('user doesnt exist');
+        finalResult.message: 'bidding fail'
+        res.json(finalResult)
+      }
+    }).catch(err => {
+      console.log('error when trying to get user in local db at bidController : ', err);
+    })
 
   }
 }
