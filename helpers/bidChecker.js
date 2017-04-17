d1e1b95e5a83d3543e986454e53f648cc1321cac
Diky Arga <dikyarga.id@gmail.com>
2017-04-17 10:40:53 +0800

    add balance checker

--- helpers/bidChecker.js
+++ helpers/bidChecker.js
@@ -1,5 +1,8 @@
 let _ = require('lodash')
+let axios = require('axios')
 let models = require('../models')
+
+let blEndPoint = 'https://api.bukalapak.com/v2/'
 
 module.exports = {
   isMoreThanHighestBid: (auctionId, nextBid) => {
@@ -14,7 +17,10 @@ module.exports = {
           let highestBid = _.maxBy(bids, 'current_bid')
           switch (true) {
             case (nextBid > highestBid.current_bid):
-              resolve(true)
+              resolve({
+                status: true,
+                message: 'Yap, tawaran lebih tinggi dari sebelumnya'
+              })
               break;
             case (nextBid == highestBid.current_bid):
               reject('Anda bid sama dengan yang di bid orang lain')
@@ -27,6 +33,32 @@ module.exports = {
         } else {
           reject('tidak auction dengan id ' + auctionId)
         }
+      })
+    })
+  },
+  checkBalance: (bukalapakId, token) => {
+    return new Promise((resolve, reject) => {
+      // cek saldo nya
+      axios({
+        method: 'get',
+        url: blEndPoint + 'dompet/history.json',
+        auth: {
+          username: bukalapakId,
+          password: token
+        }
+      }).then((responseGetBalance) => {
+        console.log('isi setelah get balance : ', responseGetBalance.data.balance);
+        resolve({
+          status: true,
+          balance: responseGetBalance.data.balance
+          message: 'get balance success'
+        })
+      }).catch(err => {
+        console.log('error when trying to get balance in bidChacker : ', err);
+        reject({
+          status: false,
+          message: 'error when trying to get balance in bidChacker : ', err
+        })
       })
     })
   }
