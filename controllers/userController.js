f360028b6a4b7f082de3712bd40f700b02bd8301
Eri Irawan <eryirawan91@gmail.com>
2017-04-18 18:32:18 +0700

    get list of auctions joined on progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 'use strict'
 const models = require('../models');
+const _ = require('lodash')
 
 module.exports = {
   auctionsJoined : (req, res) => {
@@ -22,30 +23,49 @@ module.exports = {
         }
       }]
     }).then(user => {
-      let counTemp = []
-      let count = []
-      for (var i = 0; i < user.Bids.length; i++) {
-        if (user.Bids[i].Auction.id === user.Bids[i].Auction.id) {
-          counTemp.push(user.Bids[i].Auction.id)
-        }else {
-          count.push(user.Bids[i].Auction.id)
+        let count = _.uniqBy(user.Bids, 'auctionId')
+        let wonCount = _.uniqBy(user.Bids, 'current_bid')
+            wonCount = _.maxBy(wonCount, 'current_bid')
+        let arrWonCount = []
+            arrWonCount.push(wonCount.current_bid)
+
+        let AuctionsJoined = JSON.parse(JSON.stringify(user.Bids))
+            AuctionsJoined = _.uniqBy(AuctionsJoined, 'auctionId')
+
+        let newAuctionsJoined =  AuctionsJoined.map(data => {
+          return Object.assign({}, data, {
+            auctionId: data.Auction.id,
+            title: data.Auction.title,
+            running: 19 < 18 ? true : false
+          })
+        })
+
+        for (var i = 0; i < newAuctionsJoined.length; i++) {
+          delete newAuctionsJoined[i].id
+          delete newAuctionsJoined[i].userId
+          delete newAuctionsJoined[i].current_bid
+          delete newAuctionsJoined[i].updatedAt
+          delete newAuctionsJoined[i].createdAt
+          delete newAuctionsJoined[i].current_bid
+          delete newAuctionsJoined[i].Auction
         }
-      }
-        console.log('list of acution Temp :', counTemp)
-        console.log('list of acution :', count)
+
         finalResult.success = true
         finalResult.message = 'Success load list of auction joined'
-        // finalResult.user_detail.auctionsJoinedCount = user.Bids.Auction.length
+        finalResult.user_detail.auctionsJoinedCount = count.length
         finalResult.user_detail.id = user.id
         finalResult.user_detail.name = user.name
-        finalResult.user_detail.wonAuctionsCount = 2
-        finalResult.auctionsJoined =
-        console.log('user_detail : ', finalResult);
-        res.json(user)
-      })
-      .catch(err => {
+        finalResult.user_detail.wonAuctionsCount = arrWonCount.length
+        finalResult.auctionsJoined = newAuctionsJoined
+
+        res.json(finalResult)
+      }).catch(err => {
         console.log('error when try get user by id : ', err);
         finalResult.message = `User with id ${req.params.id} not found`
       })
   }
+}
+
+function isThisUserTheWinnerOfThisAuction(userId) {
+
 }
