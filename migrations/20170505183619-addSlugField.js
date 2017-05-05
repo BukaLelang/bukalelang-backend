ccd402d852e8f472562affa4c348859024b9f6b9
Diky Arga <dikyarga.id@gmail.com>
2017-05-06 02:39:42 +0800

    add slug field in Auctions table

---
+++ migrations/20170505183619-addSlugField.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('Auctions', 'slug',{type:Sequelize.STRING})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('Auctions','slug')
+  }
+};
