672d3c20a9c9459f26cc19c0fbf1d79eedf7e5be
Diky Arga <dikyarga.id@gmail.com>
2017-05-05 23:15:36 +0800

    setup CORS for all routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 require('dotenv').config()
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -44,6 +45,8 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(cors())
 app.use(logger('dev'));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
