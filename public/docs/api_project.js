f7fa7b663a42f2967087ffe9d08b1f19f7148351
Diky Arga <dikyarga.id@gmail.com>
2017-05-13 18:14:30 +0800

    generate new docs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T12:34:03.735Z",
+    "time": "2017-05-13T10:12:08.785Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
