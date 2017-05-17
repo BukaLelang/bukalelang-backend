605b2b1238baf1dfadc4f1c84094b27def3c3b89
Diky Arga <dikyarga.id@gmail.com>
2017-05-17 09:31:02 +0700

    refactor using Promise all to handle multiple image upload

--- helpers/imageUploader.js
+++ helpers/imageUploader.js
@@ -5,29 +5,38 @@ let fs = require('fs')
 let blEndPoint = 'https://api.bukalapak.com/v2/images.json'
 
 module.exports = {
-  uploadToBukaLapak: (bukalapakId, token) => {
-    // console.log('isi bukalapakId ', bukalapakId, token);
-    return new Promise((resolve, reject) => {
+  uploadToBukaLapak: (bukalapakId, token, imagesName) => {
+      let username = bukalapakId
+      let password = token
+      let url = 'https://' + username + ':' + password + '@api.bukalapak.com/v2/images.json';
 
-      var username = bukalapakId,
-      password = token,
-      url = 'https://' + username + ':' + password + '@api.bukalapak.com/v2/images.json';
-      var formData = {
-        // Pass data via Streams
-        //  with dummy data
-        file: fs.createReadStream(__dirname + '/images/sample-image.jpg'),
-      };
-      request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
-        // console.log('Upload successful!  Server responded with:', body);
-        if (err) {
-          reject('upload failed:' + err);
+      let fn = function uploadImageToBL(imageName) {
+        return new Promise((resolve, reject) => {
+              let formData = {
+                file: fs.createReadStream(__dirname + '/images/' + imageName),
+              };
+
+              request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
+                if (err) {
+                  reject('upload failed:' + err);
+                }
+                if (body.status == 'ERROR') {
+                  reject('Error saat upload image : ' + body.message)
+                } else {
+                  // console.log('lololo : ', JSON.parse(body).id);
+                  resolve(JSON.parse(body).id)
+                }
+              });
+        })
+      }
+
+      let actions = imagesName.map(fn)
+
+      var results = Promise.all(actions);
+
+      return results.then(data => {
+          return data
         }
-        if (body.status == 'ERROR') {
-          reject('Error saat upload image : ' + body.message)
-        } else {
-          resolve(JSON.parse(body))
-        }
-      });
-    })
+      );
   }
 }
