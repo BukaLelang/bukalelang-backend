31b977c1d40ff198d3668eebde98efed4769b0f6
Diky Arga <dikyarga.id@gmail.com>
2017-05-17 14:04:40 +0700

    change api docs endpoint to HTTPS

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -2,7 +2,7 @@ define({
   "name": "BukaLelang API Docs",
   "version": "1.0.0",
   "description": "Dokumentasi API BukaLelang",
-  "url": "http://api.bukalelang.id",
+  "url": "https://api.bukalelang.id",
   "sampleUrl": "http://localhost:3000",
   "template": {
     "forceLanguage": "en"
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-16T19:12:40.764Z",
+    "time": "2017-05-17T06:56:27.207Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
