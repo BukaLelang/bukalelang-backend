ea6f47d8125168f81ab60ccfb3c8f5247c400933
Diky Arga <dikyarga.id@gmail.com>
2017-05-17 13:47:39 +0700

    make the bot working

--- robots/create-auctions.js
+++ robots/create-auctions.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
+let fs = require("fs");
 let moment = require('moment')
-var faker = require('faker');
-var chai = require('chai')
-var chaiHttp = require('chai-http')
+let faker = require('faker');
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+chai.use(chaiHttp)
 
 let app = require('../app')
 let models = require('../models')
@@ -16,37 +18,89 @@ function roundHundred(value){
    return Math.round(value/100)*100
 }
 
-let min_price = roundHundred(getRandomInt(10000, 900000))
-let max_price = roundHundred(getRandomInt(1000000, 5000000))
-let end_date = moment().add(getRandomInt(1, 7), 'days').format()
-var randomName = faker.commerce.productName();
+let end_date
 
-imageUploader.uploadToBukaLapak(
-  process.env.BUKALAPAK_ID,
-  process.env.BUKALAPAK_TOKEN).then((responseAfterUpload) => {
-  // console.log('responseAfterUpload : ', responseAfterUpload.id);
-  chai.request(serverHost).post('/auctions').send({
-    userId: '1',
-    bukalapakId: process.env.BUKALAPAK_ID,
-    token: process.env.BUKALAPAK_TOKEN,
-    title: randomName,
-    categoryId: 2,
-    new: false,
-    weight: 1000,
-    description: 'Ini cuma contoh deskripsi minimal 30 karakter lho, jika kurang akan error, maka nya saya banyak banyakain, masih kurang juga ?',
-    min_price: min_price,
-    max_price: max_price,
-    kelipatan_bid: 10000,
-    imagesId: responseAfterUpload.id,
-    end_date: end_date
-  }).end((err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // console.log('isi res : ', res.body);
-      console.log('Sukses buat lelang ----------------------------------');
-    }
-  });
-}).catch((err) => {
-  console.log('error di image uploader : ', err);
-})
+let auctions = JSON.parse(require('fs').readFileSync(require('path').resolve(__dirname, 'dummy-auctions.json'), 'utf8'));
+
+for (var i = 0; i < auctions.length; i++) {
+  // console.log(auctions[i].title);
+  auctions[i].end_date = moment().add(getRandomInt(1, 7), 'days').format();
+
+   (function(auction){
+
+       setTimeout(function() { createAuction(auction) }, 600000);
+
+   })(auctions[i]);
+
+}
+
+function createAuction(auction) {
+  imageUploader.uploadToBukaLapak(
+    process.env.BUKALAPAK_ID,
+    process.env.BUKALAPAK_TOKEN,
+    auction.images).then((responseAfterUpload) => {
+    // console.log('responseAfterUpload : ', responseAfterUpload.id);
+    chai.request(app).post('/auctions').send({
+      userId: '1',
+      bukalapakId: process.env.BUKALAPAK_ID,
+      token: process.env.BUKALAPAK_TOKEN,
+      title: auction.title,
+      categoryId: auction.categoryId,
+      new: auction.new,
+      weight: auction.weight,
+      description: auction.description,
+      min_price: auction.minPrice,
+      max_price: auction.maxPrice,
+      kelipatan_bid: auction.kelipatanBid,
+      imagesId: responseAfterUpload,
+      end_date: auction.end_date
+    }).end((err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // console.log('isi res : ', res.body);
+        if (res.body.success) {
+
+          console.log('Sukses buat lelang ----------------------------------');
+        } else {
+
+          console.log('gagal buat lelang  ----------------------------------');
+          console.log(res.body.message);
+        }
+      }
+    });
+  }).catch((err) => {
+    console.log('error di image uploader : ', err);
+  })
+}
+
+
+// imageUploader.uploadToBukaLapak(
+//   process.env.BUKALAPAK_ID,
+//   process.env.BUKALAPAK_TOKEN).then((responseAfterUpload) => {
+//   // console.log('responseAfterUpload : ', responseAfterUpload.id);
+//   chai.request(serverHost).post('/auctions').send({
+//     userId: '1',
+//     bukalapakId: process.env.BUKALAPAK_ID,
+//     token: process.env.BUKALAPAK_TOKEN,
+//     title: randomName,
+//     categoryId: 2,
+//     new: false,
+//     weight: 1000,
+//     description: 'Ini cuma contoh deskripsi minimal 30 karakter lho, jika kurang akan error, maka nya saya banyak banyakain, masih kurang juga ?',
+//     min_price: min_price,
+//     max_price: max_price,
+//     kelipatan_bid: 10000,
+//     imagesId: responseAfterUpload.id,
+//     end_date: end_date
+//   }).end((err, res) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // console.log('isi res : ', res.body);
+//       console.log('Sukses buat lelang ----------------------------------');
+//     }
+//   });
+// }).catch((err) => {
+//   console.log('error di image uploader : ', err);
+// })
