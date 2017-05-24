4bf20364bbb0341de9f35e82244326dd4678da51
Diky Arga <dikyarga.id@gmail.com>
2017-05-24 09:58:54 +0700

    create migration for product image

---
+++ migrations/20170524002355-create-product-image.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('ProductImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      smallImageUrl: {
+        type: Sequelize.STRING
+      },
+      auctionId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('ProductImages');
+  }
+};
