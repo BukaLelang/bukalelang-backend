c4dd0f254adcd615a256ec135e7963c4c2a4eb0b
Diky Arga <dikyarga.id@gmail.com>
2017-05-24 10:25:54 +0700

    regenerate docs after add basic token

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-23T14:03:17.438Z",
+    "time": "2017-05-24T03:15:14.040Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
