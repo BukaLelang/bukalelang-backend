3c326693f58c16df399577b581716643c56b1633
Diky Arga <dikyarga.id@gmail.com>
2017-05-25 13:28:26 +0700

    add controller to retrive data from file and passing to endpoint

---
+++ controllers/categoryController.js
@@ -0,0 +1,23 @@
+'use strict'
+let fs = require('fs')
+const _ = require('lodash')
+
+const models = require('../models');
+
+module.exports = {
+  index : (req, res) => {
+    let finalResult = {
+      success: false,
+      message: "Fail load categories of auction",
+      categories: []
+    }
+
+    let readFile = fs.readFileSync('dummy-data/categories.json').toString()
+    let categoriesJson = JSON.parse(readFile)
+    
+    finalResult.success = true
+    finalResult.message = 'Sukses load all categories'
+    finalResult.categories = categoriesJson.categories
+    res.json(finalResult)
+  }
+}
