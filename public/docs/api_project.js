a691770053cc9335cd694923174d1ac24027517d
Diky Arga <dikyarga.id@gmail.com>
2017-06-04 13:41:10 +0700

    update api docs after add new endpoint

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-04T03:20:28.992Z",
+    "time": "2017-06-04T06:39:25.261Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
