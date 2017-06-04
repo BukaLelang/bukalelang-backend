094956b8c3a396f82f26532716b279878030df60
Diky Arga <dikyarga.id@gmail.com>
2017-06-04 11:10:40 +0700

    update new api docs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-03T06:10:55.054Z",
+    "time": "2017-06-04T03:20:28.992Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
