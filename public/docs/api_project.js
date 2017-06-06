f88b469766b901fae294fa63ed735da4e51aa4d5
Diky Arga <dikyarga.id@gmail.com>
2017-06-06 16:18:00 +0700

    update api docs after adding checkout endpoint

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-06-05T19:11:47.933Z",
+    "time": "2017-06-06T09:14:54.754Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
